d24946d20390edc0d4e21d8e4c714f3f759447de
Саша <saniamit@mail.ru>
2021-12-14 11:09:08 +0300

    Проверка дз

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,24 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastname = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastname;
+        System.out.println(" * Задача 1 ");
+        System.out.println(" ФИО сотрудника — " + fullName);
+
+        System.out.println(" * Задача 2 ");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println(" * Задача 3 ");
+        String result = fullName.replace(" ", ";");
+        System.out.println(" Данные ФИО сотрудника для административного отдела - " + result);
+
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String chooseName = fullName2.replace('ё', 'е');
+        System.out.println(" * Задача 4 ");
+        System.out.println("Данные ФИО сотрудника - " + chooseName);
     }
 }
